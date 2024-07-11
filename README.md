6e9f2fe65c43d36cac7f23d27b2b0f15337a086b
ahmedahshlw <ahmedit5216@gmail.com>
2024-07-11 09:18:31 +0300

    4th main version

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
-# Project planning
+# HangMan Game
 
-### Game : HangMan
+## Date: 9/7/2024
 
-![alt text](./img/hangMan.png)
+### By: Ahmed Darwish
 
-### About
-Hangman is a classic word guessing game. The objective is to guess the letters that make up a secret word before the entire hangman diagram is drawn.
+#### [GitHub](https://github.com/AhmedAslw) | [LinkedIn](https://www.linkedin.com/in/ahmed-darwish-056b3a235/)
+***
+
+### ***Description***
+#### Hangman is a classic word guessing game. The objective is to guess the letters that make up a secret word before the entire hangman diagram is drawn.
+***
 
 ### User Story
-1. The game should select a random word from a predefined dictionary or word list.
-1. The game should display the word as a series of underscores, one for each letter in the word.
-1. The player should be able to guess letters by clicking on the letters of the alphabet provided.
-1. If the player guesses a correct letter, the letter should be displayed in the correct position(s) within the word.
-1. If the player guesses an incorrect letter, a part of the hangman diagram should be drawn.
-1. The game should keep track of the number of incorrect guesses, and the game should end when the entire hangman diagram is complete (indicating a loss for the player).
-1. If the player correctly guesses all the letters in the word before the hangman diagram is complete, the player should win the game.
-1. The game should provide a clear indication of the player's win or loss at the end of the game.
-1. The game should allow the player to start a new game after completing a round.
-1. The game should be responsive and work well on both desktop and mobile devices.
+1. The game should select a random word from a predefined word list.
+2. The game should display the word as a series of underscores, one for each letter in the word.
+3. The player should be able to guess letters by clicking on the letters of the alphabet provided.
+4. If the player guesses a correct letter, the letter should be displayed in the correct position(s) within the word.
+5. If the player guesses an incorrect letter, a part of the hangman diagram should be drawn.
+6. The game should keep track of the number of incorrect guesses, and the game should end when the entire hangman diagram is complete (indicating a loss for the player).
+7. If the player correctly guesses all the letters in the word before the hangman diagram is complete, the player should win the game.
+8. The game should provide a clear indication of the player's win or loss at the end of the game.
+9. The game should allow the player to start a new game after completing a round.
+10. The game should be responsive and work well on both desktop and mobile devices.
 
 ### Main Functionalities
 ```
 1. The game selects a random word and displays it as a series of underscores, one for each letter.
-2. The player clicks or taps on letters from the alphabet to guess the word.
+2. The player clicks on letters from the alphabet to guess the word.
 3. If the guessed letter is in the word, it is revealed in the correct position(s).
 4. If the guessed letter is not in the word, a part of the hangman diagram is drawn.
 5. The player continues guessing letters until they either:
@@ -36,13 +40,33 @@ Hangman is a classic word guessing game. The objective is to guess the letters t
 1 . Functions
 
 ```
-A . breakBrick()
-B . followBall()
-C . resetGame()
+A . init()     { //Initiation of the game ..reset game }
+B . RandWord() { //Start the game }
 ```
+2 . Listeners
 
-### Technologies Used
->HTML(Html Canvas), CSS, JAVASCRIPT
+```
+A . button     //Letters clicked
+B . testword   //Test button
+C . start      //Start the game
+D . reset      //Restart the game
+```
+***
+
+### ***Screenshots***
+
+##### Game before any action
+![](./img/hangMan.png)
+
+##### After clicking start
+![](./img/hangMan-start.png)
+
+##### Game over and man Hanged 
+![](./img/hangMan-gameover.png)
+
+##### After clicking start reset
+![](./img/hangMan-reset.png)
+***
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
-let keyy = [];
+// let keyy = [];
 let Rword = [
      {name:'Ahmed', hint:'Boy Name'},
     //  {name:'Ayman', hint:'Boy Name'},
     //  {name:'Hasan', hint:'Boy Name'},
     //  {name:'Denis', hint:'Boy Name'},
-    //  {name:'Abbas', hint:'Boy Name'},
+    //  {name:'Baqer', hint:'Boy Name'},
     //  {name:'Salma', hint:'Girl Name'},
     //  {name:'Nawal', hint:'Girl Name'},
-     {name:'Layla', hint:'Girl Name'}
+    //  {name:'Layla', hint:'Girl Name'},
+    //  {name:'Trout', hint:'Fish Name'},
+     {name:'Shark', hint:'Fish Name'},
 ];
 
 /*------------------------ Cached Element References ------------------------*/
 const hang = document.getElementById('hang');
 const word = document.querySelector('.word');
 const keyword = document.querySelector('.keyword');
-// const keywordd = document.getElementById('.keyword');
 const button = document.querySelectorAll('.letter');
 const win = document.querySelector('.win');
 const testword = document.querySelector('.testword');
@@ -26,99 +27,89 @@ const reset = document.querySelector('.reset');
 
 /*----------------------------- Event Listeners -----------------------------*/
 let counter = 0;
-
+let RandomW = Rword[Math.floor(Math.random()*Rword.length)];
+const idx = Rword.indexOf(RandomW);
 
 button.forEach((element) => {
     element.addEventListener('click', () => {
-        const RandomW = Rword[Math.floor(Math.random()*Rword.length)];
-        const idx = Rword.indexOf(RandomW);
         const btn = element.innerText;
-        if(btn.includes(Rword[idx].name)){
-                keyword.textContent += btn;
-                // testword.addEventListener('click', () => {
-                //     const typo = keyword.textContent;
-                //     if(typo.toLowerCase() === Rword[idx].name.toLowerCase()){
-                //          win.textContent = "Status : Win ";
-                //          keyword.textContent = '';
-                //     }
-                // });
-
-        } else if(btn != btn.includes(Rword[idx].name) && counter == 0){
-                hang.src = "./img/gibbet-head.png";
-                counter ++
-        } else if(btn != btn.includes(Rword[idx].name) && counter == 1){
-            hang.src = "./img/gibbet-head-skelleton.png";
-            counter ++
-        } else if(btn != btn.includes(Rword[idx].name) && counter == 2){
-            hang.src = "./img/gibbet-head-skelleton-oneH.png";
-            counter ++
-        } else if(btn != btn.includes(Rword[idx].name) && counter == 3){
-            hang.src = "./img/gibbet-head-skelleton-twoH.png";
-            counter ++
-        } else if(btn != btn.includes(Rword[idx].name) && counter == 4){
-            hang.src = "./img/gibbet-head-skelleton-twoH-oneL.png";
-            counter ++
-        } else if(btn != btn.includes(Rword[idx].name) && counter == 5){
-            hang.src = "./img/gibbet-head-skelleton-twoH-twoL.png";
-            counter ++
-        } else if(btn != btn.includes(Rword[idx].name) && counter == 6){
-            win.textContent = "Status : Game Over ";
-        }
+        // if(btn.includs(RandomW))
+        keyword.textContent += btn;
+    // } else if(btn != btn.includes(RandomW) && counter == 0){
+    //             hang.src = "./img/gibbet-head.png";
+    //             counter ++
+    //     } else if(btn != btn.includes(RandomW)  && counter == 1){
+    //         hang.src = "./img/gibbet-head-skelleton.png";
+    //         counter ++
+    //     } else if(btn != btn.includes(RandomW)  && counter == 2){
+    //         hang.src = "./img/gibbet-head-skelleton-oneH.png";
+    //         counter ++
+    //     } else if(btn != btn.includes(RandomW)  && counter == 3){
+    //         hang.src = "./img/gibbet-head-skelleton-twoH.png";
+    //         counter ++
+    //     } else if(btn != btn.includes(RandomW)  && counter == 4){
+    //         hang.src = "./img/gibbet-head-skelleton-twoH-oneL.png";
+    //         counter ++
+    //     } else if(btn != btn.includes(RandomW)  && counter == 5){
+    //         hang.src = "./img/gibbet-head-skelleton-twoH-twoL.png";
+    //         counter ++
+    //     } else if(btn != btn.includes(RandomW)  && counter == 6){
+    //         win.textContent = "Status : Game Over ";
+    //     }
     });
 });
-
+// console.log(keyword.textContent.toLowerCase() != keyy)
 testword.addEventListener('click', () => {
     const typo = keyword.textContent;
-    const RandomW = Rword[Math.floor(Math.random()*Rword.length)];
     const idx = Rword.indexOf(RandomW);
-    if(typo.toLowerCase() === Rword[idx].name.toLowerCase()){
+    console.log(RandomW)
+    if(typo.toLowerCase().includes(Rword[idx].name.toLowerCase())){
          win.textContent = "Status : Win ";
          keyword.textContent = '';
-    } else if (typo.toLowerCase() != Rword[idx].name.toLowerCase()){
-         win.textContent = "Status : Game Over ";
-         keyword.textContent = '';
+    } else if (!(typo.toLowerCase().includes(Rword[idx].name.toLowerCase())) && counter == 0){
          hang.src = "./img/gibbet-head.png";
-    } else {
-        win.textContent = "Status : Play";
-    }
+         counter ++
+    }  else if (!(typo.toLowerCase().includes(Rword[idx].name.toLowerCase())) && counter == 1){
+         hang.src = "./img/gibbet-head-skelleton.png";
+         counter ++
+    }  else if (!(typo.toLowerCase().includes(Rword[idx].name.toLowerCase())) && counter == 2){
+         hang.src = "./img/gibbet-head-skelleton-oneH.png";
+         counter ++
+    }  else if (!(typo.toLowerCase().includes(Rword[idx].name.toLowerCase())) && counter == 3){
+         hang.src = "./img/gibbet-head-skelleton-twoH.png";
+         counter ++
+    }  else if (!(typo.toLowerCase().includes(Rword[idx].name.toLowerCase())) && counter == 4){
+         hang.src = "./img/gibbet-head-skelleton-twoH-oneL.png";
+         counter ++
+    }  else if (!(typo.toLowerCase().includes(Rword[idx].name.toLowerCase())) && counter == 5){
+        hang.src = "./img/gibbet-head-skelleton-twoH-twoL.png";
+        win.textContent = "Status : Game Over ";
+        keyword.textContent = '';
+    }  
 })
 
 start.addEventListener('click', RandWord);
 
 reset.addEventListener('click', init);
 
-// keywordd.addEventListener('click', () => {
-//     const spliceWord = keyword.textContent.split('');
-//     console.log(spliceWord)
-// });
-
 /*-------------------------------- Functions --------------------------------*/
 
 function init (){
+    let RandomW = Rword[Math.floor(Math.random()*Rword.length)];
     keyword.textContent = '';
     win.innerText = "Status :"
-    const RandomW = Rword[Math.floor(Math.random()*Rword.length)];
-    word.textContent = `${RandomW.hint}`;
+    word.textContent = `${RandomW.hint} & first letter ${RandomW.name.slice(0,1)}`;
     hang.src = './img/gibbet.png';
     counter = 0;
 }
 
 function RandWord(){
+    let RandomW = Rword[Math.floor(Math.random()*Rword.length)];
     let StartSound = new Audio('./Audio/mixkit.wav')
     StartSound.play()
-    const RandomW = Rword[Math.floor(Math.random()*Rword.length)];
-    word.textContent = `${RandomW.hint}`;
+    word.textContent = `${RandomW.hint} & first letter ${RandomW.name.slice(0,1)}`;
     hang.src = './img/gibbet.png';
 }
-
-// function wordEntered (){
-//     const spliceWord = keyword.textContent.split('');
-    
-//         for(let i=0; i< keyy.length; i++){
-//             keyy[i] = spliceWord[i];
-//             console.log(keyy[i])
-//         }
-// }
 
 
 
